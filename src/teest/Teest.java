db920196a114c7e527f01d96763e5a98901790cf
fatiecha <fati.echarqaoui@gmail.com>
2016-12-02 11:47:55 +0000

    methode add

---
+++ src/teest/Teest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teest;
+
+/**
+ *
+ * @author Fatimzhra
+ */
+public class Teest {
+
+    /**
+     * @param args the command line arguments
+     */public static float a=2;
+       private static int b=4;
+
+    public int add(){
+        return (int) (a+b);
+    }
+    
+}
